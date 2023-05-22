db6f421e17557561585c60be41620d91ee015c9e
Romain Deflo <romain.deflo@gmail.com>
2023-05-22 10:59:00 +0200

    exercice 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'coach', to: 'pages#coach', as: :coach
+  get 'answer', to: 'pages#answer', as: :answer
 
   # Defines the root path route ("/")
   # root "articles#index"
